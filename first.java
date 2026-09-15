3b84ede0cf2e1e051df67d405e5fd34879909ed3
gvasudeva1997 <gvasudeva1997@users.noreply.github.com>
2026-09-15 13:36:46 -0400

    Initial commit: Add first.java sample program

---
+++ first.java
@@ -0,0 +1,20 @@
+
+public class first {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Variables
+        String name = "Vasu";
+        int age = 20;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+        // Simple Loop
+        System.out.println("Counting to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
